e2bf32b33be77168c186a04814165ec5225b45fb
Jesus Angel Quezada Camacho <jesusangelquezada@MacBook-Pro-de-Jesus.local>
2025-10-10 15:25:48 -0600

    Retorno unico de token

--- controllers/login.js
+++ controllers/login.js
@@ -25,18 +25,8 @@ const Logg = async (req, res) => {
         if (!match) {
             return res.status(401).json({ ok: false, msg: "Contraseña incorrecta" });
         }
-
-        const result = {
-            id_Cuenta: usuario.id_Cuenta,
-            id_Unidad_Academica: usuario.id_Unidad_Academica,
-            rfc: usuario.rfc,
-            nombre: usuario.nombre,
-            correo: usuario.correo,
-            rol: usuario.rol,
-        };
-        const token = jwt.signJwt({ id_Cuenta: usuario.id_Cuenta, rol: usuario.rol , id_Unidad_Academica: usuario.id_Unidad_Academica, nombre: usuario.nombre });
-        result.token = token;
-        return res.status(200).json({ ...result, token });
+        const result = jwt.signJwt({ id_Cuenta: usuario.id_Cuenta, rol: usuario.rol , id_Unidad_Academica: usuario.id_Unidad_Academica, nombre: usuario.nombre, correo: usuario.correo });
+        return res.status(200).json({ token: result });
     } catch (err) {
         console.log(err);
         return res.status(500).json({ ok: false, msg: 'Algo salió mal' });
