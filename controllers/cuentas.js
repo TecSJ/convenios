88dded5fa01ff04f3650d1e20a6ad9406fd7fc03
Uriel Aguilera <uriel.aguilera@tecmm.edu.mx>
2025-10-20 11:26:17 -0600

    endpoint registrar cuenta como coordinador

--- controllers/cuentas.js
+++ controllers/cuentas.js
@@ -4,11 +4,11 @@ const Mailer = require("../helpers/correoService");
 
 const createCuenta = async (req, res) => {
     const { nombre, correo, contrasena, rol, rfc, id_Unidad_Academica } = req.body;
-    const con = await db.getConnection();
     const X_API_KEY = req.headers['api_key'];
     if (X_API_KEY !== process.env.X_API_KEY) {
         return res.status(401).json({ ok: false, msg: 'Falta api key' });
     }
+    const con = await db.getConnection();
     try {
         if (!nombre || !correo || !contrasena || !rol || !rfc || !id_Unidad_Academica) {
             return res.status(400).json({ ok: false, msg: "Todos los campos son requeridos" });
@@ -57,11 +57,11 @@ const createCuenta = async (req, res) => {
 
 const restorePass = async (req, res) => {
     const { correo } = req.body;
-    const con = await db.getConnection();
     const X_API_KEY = req.headers['api_key'];
     if (X_API_KEY !== process.env.X_API_KEY) {
         return res.status(401).json({ ok: false, msg: 'Falta api key' });
     }
+    const con = await db.getConnection();
     try {
         if (!correo) {
             return res.status(400).json({ ok: false, msg: "El correo es requerido" });
@@ -108,7 +108,76 @@ const restorePass = async (req, res) => {
     }
 };
 
+const createCuentasAdmin = async (req, res) => {
+    const {nombre, correo, rol, rfc, id_Unidad_Academica} = req.body;
+
+    const con = await db.getConnection();
+
+    try {
+        if (!nombre || !correo || !rol || !rfc || !id_Unidad_Academica) {
+            return res.status(400).json({ ok: false, msg: "Todos los campos son requeridos" });
+        }
+        if (!['Gestor','Organización','Coordinador','Revisor','Director Unidad','Director General'].includes(rol)) {
+            return res.status(400).json({ ok: false, msg: "Rol inválido" });
+        }
+        if (rfc.length !== 13 || rfc.includes(' ')) {
+            return res.status(400).json({ ok: false, msg: "El RFC debe tener 13 caracteres" });
+        }
+        const [unidad] = await con.query(
+            "SELECT * FROM Unidades_Academicas WHERE id_Unidad_Academica = ?",
+            [id_Unidad_Academica]
+        );
+        if (unidad.length === 0) {
+            return res.status(400).json({ ok: false, msg: "La unidad académica no existe" });
+        }
+        
+        const [existingUsers] = await con.query(
+            "SELECT * FROM Cuentas WHERE correo = ?",
+            [correo]
+        );
+
+        if (existingUsers.length > 0) {
+            return res.status(409).json({ ok: false, msg: "El correo ya está en uso" });
+        }
+
+        const nuevaContrasena = Math.random().toString(36).slice(-10);
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(nuevaContrasena, salt);
+
+        const [result] = await con.query(
+            "INSERT INTO Cuentas (nombre, correo, contrasena, rol, rfc, id_Unidad_Academica, estado) VALUES (?, ?, ?, ?, ?, ?, 'Activo')",
+            [nombre, correo, hashedPassword, rol, rfc, id_Unidad_Academica]
+        );
+
+        const mailer = new Mailer();
+        const asunto = 'Cuenta de acceso';
+        const contenido = `
+            <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
+            <div style="max-width: 600px; margin: auto; background: #fff; padding: 20px; border-radius: 8px; text-align: center;">
+                <h1 style="color: #333;">Cuenta de acceso </h1>
+                <p style="font-size: 16px; color: #555;">Tu usuario es:</p>
+                <p style="font-size: 20px; font-weight: bold; color: #007BFF;">${correo}</p>
+                <p style="font-size: 16px; color: #555;">Tu contraseña es:</p>
+                <p style="font-size: 20px; font-weight: bold; color: #007BFF;">${nuevaContrasena}</p>
+            </div>
+            </body>
+        `;
+
+        await mailer.enviarCorreo(correo, asunto, contenido);
+
+        return res.status(201).json({ ok: true, msg: "Cuenta creada exitosamente"});
+
+    }catch(error){
+        console.log(err);
+        return res.status(500).json({ ok: false, msg: 'Algo salió mal' });
+    }finally{
+        con.release();
+    }
+    
+}
+
 module.exports = {
     createCuenta,
-    restorePass
+    restorePass,
+    createCuentasAdmin
 }
--- router/cuentas.js
+++ router/cuentas.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { createCuenta, restorePass } = require("../controllers/cuentas");
+const { createCuenta, restorePass, createCuentasAdmin } = require("../controllers/cuentas");
+const authMiddleware = require("../middlewares/authMiddleware");
 const routerCuentas = Router();
 
 routerCuentas.patch('/',createCuenta);
 routerCuentas.patch('/restore',restorePass);
+routerCuentas.post('/admin', authMiddleware, createCuentasAdmin);
 
 module.exports = (app) => app.use('/cuenta',routerCuentas);
